35451b8a97419e30ab1c9c3b82da425db64034d8
yasirlashari <yasirlashari131@gmail.com>
2023-06-21 19:22:45 -0700

    Ab navbar bih add krdya

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Routes,Route } from "react-router-dom";
-import { Home } from "./Components/Home";
-import { About } from "./Components/About";
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './Components/Home';
+import { About } from './Components/About';
+import { Navbar } from './Components/Navbar';
+
 function App() {
   return (
-<Routes>
-  <Route path='/' element={<Home/>}></Route>
-  <Route path='About' element={<About/>}></Route>
-</Routes>
+    <>
+    <Navbar/>
+    <Routes>
 
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    </>
   );
 }
 
